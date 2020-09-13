875a5d69d0ebd98d41872f2f6eac216ecae7149f
Chujun Dong <chdo@microsoft.com>
2020-09-13 18:46:46 +0800

    add api call for compute vision

---
+++ app/analyzeImage.js
@@ -0,0 +1,37 @@
+const request = require('request');
+
+let subscriptionKey = "25b86915fc604618a76146b62d1bda53";
+let endpoint = "https://computevision913.cognitiveservices.azure.com/";
+if (!subscriptionKey) { throw new Error('Set your environment variables for your subscription key and endpoint.'); }
+
+var uriBase = endpoint + 'vision/v3.0/analyze';
+
+const imageUrl =
+    'https://upload.wikimedia.org/wikipedia/commons/3/3c/Shaki_waterfall.jpg';
+
+// Request parameters.
+const params = {
+    'visualFeatures': 'Categories,Description,Color',
+    'details': '',
+    'language': 'en'
+};
+
+const options = {
+    uri: uriBase,
+    qs: params,
+    body: '{"url": ' + '"' + imageUrl + '"}',
+    headers: {
+        'Content-Type': 'application/json',
+        'Ocp-Apim-Subscription-Key' : subscriptionKey
+    }
+};
+
+request.post(options, (error, response, body) => {
+  if (error) {
+    console.log('Error: ', error);
+    return;
+  }
+  let jsonResponse = JSON.stringify(JSON.parse(body), null, '  ');
+  console.log('JSON Response\n');
+  console.log(jsonResponse);
+});
